d5ef530e5f2132165d1e100d0315b6253a808baa
PHELIPE STERING <68488889+phelipestering@users.noreply.github.com>
2022-01-16 11:01:09 -0300

    ESTUDANDO O REQUEST

--- public/formularios/form.php
+++ public/formularios/form.php
@@ -1,16 +1,40 @@
 <?php
 
-echo "Nome: {$_POST['nome']}";
+// O REQUEST SERVE PARA PEGAR OS DADOS DE REQUISICOES DE TODOS OS TIPOS
+
+// SEJA POST, GET OU ANY
+
+
+
+echo "Nome: {$_REQUEST['nome']}";
 echo '<hr>';
-echo "Senha: {$_POST['senha']}";
+echo "Senha: {$_REQUEST['senha']}";
 echo '<hr>';
-echo "Email: {$_POST['email']}";
+echo "Email: {$_REQUEST['email']}";
 echo '<hr>';
-echo "Concorda com os termos".isset($_POST['concordo']) &&  $_POST['concordo'] != '' ? 'CONCORDA COM OS TERMOS' : ' TERMO NAO ACEITADO';
+echo "Concorda com os termos".isset($_REQUEST['concordo']) &&  $_REQUEST['concordo'] != '' ? 'CONCORDA COM OS TERMOS' : ' TERMO NAO ACEITADO';
 echo '<hr>';
-echo "Sexo : {$_POST['sexo']}";
+echo "Sexo : {$_REQUEST['sexo']}";
 echo '<hr>';
-echo "Estado: {$_POST['estado']}";
+echo "Estado: {$_REQUEST['estado']}";
 echo '<hr>';
-echo "Descricao: {$_POST['descricao']}";
+echo "Descricao: {$_REQUEST['descricao']}";
 echo '<hr>';
+
+
+// RECUPERANDO PELO METODO POST
+
+// echo "Nome: {$_POST['nome']}";
+// echo '<hr>';
+// echo "Senha: {$_POST['senha']}";
+// echo '<hr>';
+// echo "Email: {$_POST['email']}";
+// echo '<hr>';
+// echo "Concorda com os termos".isset($_POST['concordo']) &&  $_POST['concordo'] != '' ? 'CONCORDA COM OS TERMOS' : ' TERMO NAO ACEITADO';
+// echo '<hr>';
+// echo "Sexo : {$_POST['sexo']}";
+// echo '<hr>';
+// echo "Estado: {$_POST['estado']}";
+// echo '<hr>';
+// echo "Descricao: {$_POST['descricao']}";
+// echo '<hr>';
