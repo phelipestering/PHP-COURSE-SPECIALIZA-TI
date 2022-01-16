28a48f1b855415d993bcf321cba783a28ab91f26
PHELIPE STERING <68488889+phelipestering@users.noreply.github.com>
2022-01-16 11:39:20 -0300

    Update form.php
    VALIDACAO DE FORMULARIO - METODO 2

--- public/formularios/form.php
+++ public/formularios/form.php
@@ -4,39 +4,27 @@
 
 // SEJA POST, GET OU ANY
 
-if ($_REQUEST['nome'] != '' && strlen($_REQUEST['nome']) > 3 ){
+if ($_REQUEST['nome'] == '' || strlen($_REQUEST['nome']) <= 3 ){
     
-    echo "Nome: {$_REQUEST['nome']}";
-    echo '<hr>';
-    echo "Senha: {$_REQUEST['senha']}";
-    echo '<hr>';
-    echo "Email: {$_REQUEST['email']}";
-    echo '<hr>';
-    echo "Concorda com os termos".isset($_REQUEST['concordo']) &&  $_REQUEST['concordo'] != '' ? 'CONCORDA COM OS TERMOS' : ' TERMO NAO ACEITADO';
-    echo '<hr>';
-    echo "Sexo : {$_REQUEST['sexo']}";
-    echo '<hr>';
-    echo "Estado: {$_REQUEST['estado']}";
-    echo '<hr>';
-    echo "Descricao: {$_REQUEST['descricao']}";
-    echo '<hr>';
-}else {
-    echo 'O campo nao cumpre os requisitos';
-}
+    echo 'o campo e invalido';
+
+    return;
+
+    } 
 
 // RECUPERANDO PELO METODO POST
 
-// echo "Nome: {$_POST['nome']}";
-// echo '<hr>';
-// echo "Senha: {$_POST['senha']}";
-// echo '<hr>';
-// echo "Email: {$_POST['email']}";
-// echo '<hr>';
-// echo "Concorda com os termos".isset($_POST['concordo']) &&  $_POST['concordo'] != '' ? 'CONCORDA COM OS TERMOS' : ' TERMO NAO ACEITADO';
-// echo '<hr>';
-// echo "Sexo : {$_POST['sexo']}";
-// echo '<hr>';
-// echo "Estado: {$_POST['estado']}";
-// echo '<hr>';
-// echo "Descricao: {$_POST['descricao']}";
-// echo '<hr>';
+echo "Nome: {$_POST['nome']}";
+echo '<hr>';
+echo "Senha: {$_POST['senha']}";
+echo '<hr>';
+echo "Email: {$_POST['email']}";
+echo '<hr>';
+echo "Concorda com os termos".isset($_POST['concordo']) &&  $_POST['concordo'] != '' ? 'CONCORDA COM OS TERMOS' : ' TERMO NAO ACEITADO';
+echo '<hr>';
+echo "Sexo : {$_POST['sexo']}";
+echo '<hr>';
+echo "Estado: {$_POST['estado']}";
+echo '<hr>';
+echo "Descricao: {$_POST['descricao']}";
+echo '<hr>';
