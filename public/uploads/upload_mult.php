0ce34e3862c4b2b03898feed1cadca4fcaf749dc
PHELIPE STERING <68488889+phelipestering@users.noreply.github.com>
2022-01-16 22:42:50 -0300

    UPLOAD DE VARIOS ARQUIVOS - CRIACAO DE PASTAS E VERIFICACAO

---
+++ public/uploads/upload_mult.php
@@ -0,0 +1,19 @@
+<?php
+
+$photos = $_FILES['image'];
+
+$path = './imgs/';
+
+if(!is_dir($path)){
+    mkdir('uploads');
+    mkdir($path);
+}
+
+for ($i = 0; $i < count($photos['name']); $i++){
+    $j = $i + 1;
+    if (move_uploaded_file($photos['tmp_name'][$i], $path . $photos['name'][$i]))
+        echo "Sucesso ao mover o arquivo {$j} <br>";
+
+    else
+        echo "Falha ao mover o arquivo {$j}";
+}
